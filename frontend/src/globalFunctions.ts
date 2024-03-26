53ef436ba2b4f53607a9cf0ccd8d938dff0e1a6e
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-26 16:21:47 +0100

    Added secret to workflow

--- frontend/src/globalFunctions.ts
+++ frontend/src/globalFunctions.ts
@@ -3,6 +3,7 @@ import { Hello } from './types';
 export const fetchHelloWorld = async (): Promise<Hello[]> => {
   const baseUrl: string = import.meta.env.VITE_API_BASE_URL;
   const url = baseUrl + '/api/inspections';
+  console.log(url);
 
   const response: Response = await fetch(url);
   const greetings: Promise<Hello[]> = await response.json();
