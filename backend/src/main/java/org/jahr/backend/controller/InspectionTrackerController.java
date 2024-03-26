89d66f5d0d6c1c9516aade61808889f937ae317c
SoderbergHampus <soderberg.dev@gmail.com>
2024-03-26 10:06:58 +0100

    Added dummy database for testing of deployement

--- backend/src/main/java/org/jahr/backend/controller/InspectionTrackerController.java
+++ backend/src/main/java/org/jahr/backend/controller/InspectionTrackerController.java
@@ -1,20 +1,29 @@
 package org.jahr.backend.controller;
 
+import org.jahr.backend.model.HelloWorld;
+import org.jahr.backend.repository.HelloWorldRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/api/inspections")
 public class InspectionTrackerController {
 
-    public InspectionTrackerController() {}
+    private final HelloWorldRepository repo;
+
+    public InspectionTrackerController(HelloWorldRepository repo) {
+        this.repo = repo;
+    }
 
     @GetMapping
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("Hello World");
+    public ResponseEntity<List<HelloWorld>> helloWorld() {
+        repo.save(new HelloWorld("Hello world"));
+        return ResponseEntity.ok(repo.findAll());
     }
 }
---
+++ backend/src/main/java/org/jahr/backend/model/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.jahr.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class HelloWorld {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String msg;
+
+    public HelloWorld() {
+    }
+
+    public HelloWorld(String msg) {
+        this.msg = msg;
+    }
+}
