34169c1001c8c9d62c75ea646cda0ca19cfde947
JagodaBodnar <jagodabodnar90@gmail.com>
2024-03-27 12:44:01 +0100

    FE-Create_form_fields_name_date_location_area - fields with mocked data + basic styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from 'react';
-import { fetchHelloWorld } from './globalFunctions';
-import { Hello } from './types';
+import InspectionForm from "./components/InspectionForm.tsx";
+
 
 const App = () => {
-  const [greetings, setGreetings] = useState<Hello[]>();
-
-  useEffect(() => {
-    fetchHelloWorld()
-      .then((res) => setGreetings(res))
-      .catch(() => console.log('Error while fetching'));
-  }, []);
 
   return (
     <>
-      {
-        greetings !== undefined && greetings.map(el => {
-          return (
-            <div key={el.id}>{el.msg}</div>
-          )
-        })
-      }
+      <InspectionForm/>
     </>
   )
 }
