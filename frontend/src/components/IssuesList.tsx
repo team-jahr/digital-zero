7798d4523d2198fdfe83f41b834d658a32bd480b
JagodaBodnar <jagodabodnar90@gmail.com>
2024-03-28 17:46:14 +0100

    FE-Prettier refactor

--- frontend/src/components/IssuesList.tsx
+++ frontend/src/components/IssuesList.tsx
@@ -1,15 +1,14 @@
-import SingleIssue from "./SingleIssue.tsx";
-import {Issue} from "../types/types.ts";
+import SingleIssue from './SingleIssue.tsx';
+import { Issue } from '../types/types.ts';
+
 type ListOfIssuesProp = {
   list: Issue[];
-}
-const IssuesList = ({list}:ListOfIssuesProp) => {
+};
+const IssuesList = ({ list }: ListOfIssuesProp) => {
   return (
     <div>
-      {list.map(el=>{
-        return(
-          <SingleIssue data={el} key={el.id}/>
-        )
+      {list.map((el) => {
+        return <SingleIssue data={el} key={el.id} />;
       })}
     </div>
   );
--- frontend/src/components/SingleIssue.tsx
+++ frontend/src/components/SingleIssue.tsx
@@ -1,44 +1,44 @@
-import {Issue} from "../types/types.ts";
-import './SingleIssueStyle.css'
-import PopUp from "./PopUp.tsx";
-import {FormEvent, useState} from "react";
-import {Button} from "antd";
-import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
+import { Issue } from '../types/types.ts';
+import './SingleIssueStyle.css';
+import PopUp from './PopUp';
+import { FormEvent, useState } from 'react';
+import { Button } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 type SingleIssueProp = {
   data: Issue;
-}
-const SingleIssue = ({data}: SingleIssueProp) => {
+};
+const SingleIssue = ({ data }: SingleIssueProp) => {
   const [isModalOpen, setModalOpen] = useState(false);
   const handleDelete = (e: FormEvent) => {
     e.preventDefault();
-    setModalOpen(true)
-  }
+    setModalOpen(true);
+  };
   const handleEdit = (e: FormEvent) => {
     e.preventDefault();
     console.log('edit');
-  }
+  };
   return (
     <>
-      <div className="single-issue-container">
-        <h1 className="text-sm font-extralight">{data.title}</h1>
-        <div className="buttons-container">
+      <div className='single-issue-container'>
+        <h1 className='text-sm font-extralight'>{data.title}</h1>
+        <div className='buttons-container'>
           <Button
             type='default'
-            shape="circle"
-            icon={<DeleteOutlined/>}
+            shape='circle'
+            icon={<DeleteOutlined />}
             onClick={handleDelete}
           />
 
           <Button
             type='default'
-            shape="circle"
-            icon={<EditOutlined/>}
+            shape='circle'
+            icon={<EditOutlined />}
             onClick={handleEdit}
           />
         </div>
       </div>
-      <PopUp open={isModalOpen} setModalOpen={setModalOpen}/>
+      <PopUp open={isModalOpen} setModalOpen={setModalOpen} />
     </>
   );
 };
