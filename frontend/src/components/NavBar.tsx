e34477d4a786e0659bb2164c978d855302fbd34a
Anthony33720 <raynaud.anthony@gmail.com>
2024-04-04 14:04:24 +0200

    logo in navbar plus style

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
+import checkMarkLogo from '../assets/check-mark.png';
 import './NavBar.css';
 
 const NavBar = () => {
@@ -7,12 +8,15 @@ const NavBar = () => {
   return (
     <nav>
       <ul className='nav-list'>
-        <li>
-          <Link to='/'>Inspection tracker</Link>
-        </li>
-        <li className={location.pathname === '/about' ? 'active' : ''}>
+        <div>
+          <Link to='/' className='nav-title'>
+            <img src={checkMarkLogo} alt='logo' className='logo-navbar' />
+            Inspection tracker
+          </Link>
+        </div>
+        <div className={location.pathname === '/about' ? 'active' : ''}>
           <Link to='/about'>About</Link>
-        </li>
+        </div>
       </ul>
       <div className='nav-user'>Johan Hedberg</div>
     </nav>
